37d9bc91bd26829b075cafabb93278855d6b9eea
Denis Cherepanov <cherepanov.denis94@mail.ru>
2017-10-12 23:58:34 +0300

    Adding the OpenTok Android SDK

--- app/src/main/java/com/cherepanov/testtaskapp/MainActivity.java
+++ app/src/main/java/com/cherepanov/testtaskapp/MainActivity.java
@@ -3,6 +3,21 @@ package com.cherepanov.testtaskapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
+
+import com.opentok.android.Session;
+import com.opentok.android.Stream;
+import com.opentok.android.Publisher;
+import com.opentok.android.PublisherKit;
+import com.opentok.android.Subscriber;
+import com.opentok.android.OpentokError;
+
+import android.support.annotation.NonNull;
+import android.Manifest;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
